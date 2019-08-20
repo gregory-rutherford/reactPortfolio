33b2bf7246adc5365adf5e7899ee6bc715ed7f5c
Gregory Rutherford <46570985+gregory-rutherford@users.noreply.github.com>
2019-08-19 21:06:01 -0500

    made the main page component

---
+++ client/src/Pages/Main.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+const Main = function Main() {
+    const [state, setState] = useState([]);
+
+    return (
+      <main>
+        <img href="https://avatars1.githubusercontent.com/u/46570985?s=460&v=4" />
+        <h4>
+          Hi, my name is Gregory Rutherford. I am a front end web developer.
+        </h4>
+      </main>
+    );
+};
+
+export default Main;
