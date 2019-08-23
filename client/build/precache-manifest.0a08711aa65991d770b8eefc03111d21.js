cab7e973160db25f905154f1e276d913a6f0a98e
Gregory Rutherford <46570985+gregory-rutherford@users.noreply.github.com>
2019-08-23 17:21:30 -0500

    added build folder

---
+++ client/build/precache-manifest.0a08711aa65991d770b8eefc03111d21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc253d9d8ec6b938193ac3294ab2f6f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daa4dbe49e21badc0146",
+    "url": "/static/css/main.06f92682.chunk.css"
+  },
+  {
+    "revision": "21fb8f86653d3cc72569",
+    "url": "/static/js/2.baa67d36.chunk.js"
+  },
+  {
+    "revision": "daa4dbe49e21badc0146",
+    "url": "/static/js/main.58311b73.chunk.js"
+  },
+  {
+    "revision": "90e9ad774bf1ba3beee1",
+    "url": "/static/js/runtime~main.e1057352.js"
+  }
+]);
